300a50880e32f8199169570f7ea3d063dc079483
David Brown <david_a_brown@mac.com>
2017-11-12 15:26:13 +0000

    Use node adapter for RequireJS
    Instead of using a statically included version of RequireJS, we now use
    the official node package instead.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+var requirejs = require('requirejs');
+
+requirejs.config({
+  //Pass the top-level main.js/index.js require
+  //function to requirejs so that node modules
+  //are loaded relative to the top-level JS file.
+  nodeRequire: require
+});
+
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
